79bd72abe24e70106595ead593e39d4db717b3cb
Tadaki741 <94947002+Tadaki741@users.noreply.github.com>
2022-12-29 19:25:48 +0700

    Initial commit

---
+++ src/main/java/com/example/hirememicroserviceCV/HireMeMicroserviceCvApplication.java
@@ -0,0 +1,13 @@
+package com.example.hirememicroserviceCV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HireMeMicroserviceCvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HireMeMicroserviceCvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hirememicroserviceCV/HireMeMicroserviceCvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hirememicroserviceCV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HireMeMicroserviceCvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
