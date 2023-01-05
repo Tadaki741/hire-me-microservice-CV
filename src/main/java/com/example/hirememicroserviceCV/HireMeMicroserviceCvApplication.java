76fa585cf51a2aeb9acb699c80f5c4ded95549b4
unknown <trankhoa758@gmail.com>
2023-01-05 23:28:45 +0700

    Remove Firebase Setup

--- src/main/java/com/example/hirememicroserviceCV/HireMeMicroserviceCvApplication.java
+++ src/main/java/com/example/hirememicroserviceCV/HireMeMicroserviceCvApplication.java
@@ -1,15 +1,8 @@
 package com.example.hirememicroserviceCV;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import java.io.FileNotFoundException;
-import java.io.InputStream;
 import java.util.logging.Logger;
 
 @SpringBootApplication
